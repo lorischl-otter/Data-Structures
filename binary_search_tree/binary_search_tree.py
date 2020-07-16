1d67f6677c435ff4219f56012ef9e78a88caac28
Lori Schlatter <lori.schlatter@gmail.com>
2020-07-16 17:20:28 -0500

    Completed module 4 BST implementation, with stretch, all tests passing.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -59,15 +59,29 @@ class BSTNode:
         # set initial max value based on root
         max_value = self.value
         # move to higher value to the right
-        current = self.right
+        current = self.right  # or current = self
         # traverse to the farthest right node
-        while current:
+        while current:  # or while current is not None
             max_value = current.value
             current = current.right
         return max_value
 
+    # better iterative in class
+    # def get_max(self)
+    #     current = self
+    #     while current.right is not None:
+    #         current = current.right
+    #     return current.value
+
+    # Recursive solution covered in lecture
+    # def get_max(self):
+    #     if not self.right:
+    #         return self.value
+    #     return self.right.get_max()
+
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
+        # depth-first traversal
         # call fn on root / current value
         fn(self.value)
         # call for_each on non-empty left
@@ -81,37 +95,97 @@ class BSTNode:
         else:
             self.right.for_each(fn)
 
-        # fn(self.left), for_each(self.left, fn) if self.left is not None
-        # # call fn & for_each on non-empty right
-        # fn(self.right), for_each(self.right, fn) if self.right is not None
+    # better recursive iteration in class
+    # this is an example of depth-first traversal
+    # def for_each(self, fn):
+    #     fn(self.value)
+    #     if self.left:
+    #         self.left.for_each(fn)
+    #     if self.right:
+    #         self.right.for_each(fn)
+
+    def iterative_breadth_first_for_each(self, fn):
+        from collections import dequeue
+        # BFT: FIFO traversal
+        # we'll use a queue structure to facilitate ordering
+        # pattern the same as before, except using a queue
+        queue = dequeue()
+        queue.append(self)
+
+        while len(queue) > 0:
+            current = queue.popleft()  # equivalent of dequeueing
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+            fn(current.value)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
+
     def in_order_print(self, node):
-        pass
+        # create an empty list
+        all_values = []
+        # create an appending function
+        fn = lambda x: all_values.append(x)
+        # us the for each method to get all numbers into a list
+        self.for_each(fn)
+        # print the sorted list
+        all_values = sorted(all_values)
+        for value in all_values:
+            print(value)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        from collections import deque
+        queue = deque()
+        queue.append(node)
+        while len(queue) > 0:
+            current = queue.popleft()
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+            print(current.value)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = []
+        stack.append(node)
+        while len(stack) > 0:
+            current = stack.pop()
+            if current.right:
+                stack.append(current.right)
+            if current.left:
+                stack.append(current.left)
+            print(current.value)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
     def pre_order_dft(self, node):
-        pass
+        # same implementation as in_order_print, without sorting values
+        all_values = []
+        fn = lambda x: all_values.append(x)
+        self.for_each(fn)
+        for value in all_values:
+            print(value)
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
-        pass
-
+        # modify implementation of for_each, to be post_order
+        all_values = []
+        if self.left:
+            self.left.post_order_dft(node)
+        if self.right:
+            self.right.post_order_dft(node)
+        all_values.append(self.value)
+        for value in all_values:
+            print(value)
 
 # breakpoint()
